6b73cec61b10f26df5a73c9cb3816186765e112b
Patrick <pat.rodriguez108@gmail.com>
2018-01-10 17:25:10 -0600

    Add newest bike prototype function to station object

--- src/station.js
+++ src/station.js
@@ -3,3 +3,12 @@ var Station = function(name, location, bikes = []) {
 	this.location = location;
 	this.bikes = bikes
 }
+
+Station.prototype.newestBike = function() {
+	var newestBike
+	for(var i = 0; i < this.bikes.length; i++) {
+		if (this.bikes[i].modelYear > this.bikes[i + 1].modelYear) {
+			return this.bikes[i]
+		};
+	};
+};
