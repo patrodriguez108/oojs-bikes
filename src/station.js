3775cff3bca7b11f6ced49125a4237d3cb9ddda3
Patrick <pat.rodriguez108@gmail.com>
2018-01-10 17:26:51 -0600

    Add else if statement

--- src/station.js
+++ src/station.js
@@ -8,7 +8,10 @@ Station.prototype.newestBike = function() {
 	var newestBike
 	for(var i = 0; i < this.bikes.length; i++) {
 		if (this.bikes[i].modelYear > this.bikes[i + 1].modelYear) {
-			return this.bikes[i]
+			return this.bikes[i];
+		} else if (this.bikes[i + 1] > this.bikes[i]) {
+			return this.bikes[i + 1]
 		};
 	};
 };
+
